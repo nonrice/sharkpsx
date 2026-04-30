e66a80650c9d43790f4db61bb80956bdaece009a
Eric Xue <widefin@gmail.com>
2026-04-30 01:44:24 -0400

    write some doc for bitfield

--- src/BitField.hpp
+++ src/BitField.hpp
@@ -8,6 +8,28 @@
 
 namespace pse {
 
+// it's an abstraction for bitfields
+//
+// i made Instr before this, so it's probably the only thing
+// now that doesn't use it
+//
+// i will overhaul it with this sometime. imm16 casing will be a problem
+// though since I had originally returned u16 for the u32.
+//
+// usage
+// note indices 0 indexed
+// union Reg {
+//     u32 val;
+//
+//     BitField<u32, 15, 31> upper_half;
+// };
+//
+// very simple
+// Reg r1{0xFFFFFFFF};
+// r1.upper_half = 0xABCD;
+// assert(r1.upper_half == 0xABCD);
+//
+//
 template <typename T, usize Start, usize End>
 requires(
     (std::same_as<T, u8> || std::same_as<T, u16> || std::same_as<T, u32>) &&
