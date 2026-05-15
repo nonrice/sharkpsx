3706d776505d426b49f93161caf585012357c0f3
Eric Xue <widefin@gmail.com>
2026-05-14 21:13:09 -0400

    more gte stuff

--- src/GTE.cpp
+++ src/GTE.cpp
@@ -1,5 +1,6 @@
 #include "GTE.hpp"
 #include "types.hpp"
+#include "Panic.hpp"
 
 namespace pse {
 
@@ -28,54 +29,181 @@ const std::array<GTE::OpHandlerPtr, 32> GTE::m_op_table = {{
     [0x1A] = &GTE::op_GPL
 }};
 
+
+constexpr u32 GTE::Regs::regname_ind(RegName r){
+    return r & REGNAME_IND_MASK;
+}
+
+constexpr bool GTE::Regs::regname_is_16(RegName r){
+    return r & REGNAME_16;
+}
+
+constexpr bool GTE::Regs::regname_is_16lo(RegName r){
+    return r & REGNAME_16LO;
+}
+
+constexpr bool GTE::Regs::regname_is_16hi(RegName r){
+    return r & REGNAME_16HI;
+}
+
+constexpr bool GTE::Regs::regname_is_u(RegName r){
+    return r & REGNAME_U;
+}
+
+constexpr bool GTE::Regs::regname_eq(RegName a, RegName b){
+    return regname_ind(a) == regname_ind(b);
+}
+
+constexpr u32 GTE::Regs::get(RegName r){
+    return raw[regname_ind(r)];
+}
+
+constexpr void GTE::Regs::set(RegName r, u32 val){
+    raw[regname_ind(r)] = val;
+}
+
 constexpr u32 GTE::Regs::read(GTE::Regs::RegName r){
-    if (r & REGNAME_PACK16) {
-        if (r & REGNAME_PACK16_IS_HI){
-            return raw[r] >> 16;
+    if (regname_is_16(r)) {
+        u16 val;
+        if (r & REGNAME_16HI){
+            val = get(r) >> 16;
         } else {
-            return raw[r] & 0xFFFF;
+            val = get(r) & 0xFFFF;
+        }
+
+        if (regname_is_u(r)){
+            return val;
+        } else {
+            return static_cast<s16>(val);
         }
     } else {
-        return raw[r];
+        if (regname_is_u(r)){
+            return get(r);
+        } else {
+            return static_cast<s32>(get(r));
+        }
     }
 }
 
-constexpr void GTE::Regs::shift_SXY(){
-    raw[SXY0] = raw[SXY1];
-    raw[SXY1] = raw[SXY2];
-}
-
-constexpr void GTE::Regs::shift_SZ(){
-    raw[SZ0] = raw[SZ1];
-    raw[SZ1] = raw[SZ2];
-    raw[SZ2] = raw[SZ3];
+constexpr u64 GTE::Regs::read64(RegName r){
+    u32 val = read(r);
+    if (regname_is_u(r)){
+        return val;
+    } else {
+        return static_cast<s32>(val);
+    }
 }
 
 constexpr void GTE::Regs::write(GTE::Regs::RegName r, u32 val){
+    if (r == FLAG){
+        Panic("Trying to write to GTE FLAG");
+    }
+
     if (r == SXYP){
-        shift_SXY();
+        shift_SXYP();
         return;
     }
 
-    if (r & REGNAME_PACK16) {
-        Pack16 reg{raw[r]};
-        if (r & REGNAME_PACK16_IS_HI){
+    if (r & REGNAME_16) {
+        Pack16 reg{ get(r) };
+        if (r & REGNAME_16HI){
             reg.lo = val;
         } else {
             reg.hi = val;
         }
-        raw[r] = reg.val;
+        set(r, reg.val);
 
     } else {
-        raw[r] = val;
+        set(r, val);
     }
 }
 
-void GTE::op_MVMVA(GTE::Instr i){
-
-
+constexpr void GTE::Regs::shift_SXYP(){
+    shift_SXY2();
+    set(SXY2, get(SXYP));
 }
 
+constexpr void GTE::Regs::shift_SXY2(){
+    set(SXY0, get(SXY1));
+    set(SXY1, get(SXY2));
+}
+
+constexpr void GTE::Regs::shift_SZ3(){
+    set(SZ0, get(SZ1));
+    set(SZ1, get(SZ2));
+    set(SZ2, get(SZ3));
+}
+
+constexpr void GTE::Regs::shift_RGBC(){
+    set(RGB0, get(RGB1));
+    set(RGB1, get(RGB2));
+    set(RGB2, get(RGBC));
+}
+
+constexpr bool GTE::Regs::get_flag(u8 i){
+    return (get(FLAG) >> i) & 1;
+}
+
+constexpr void GTE::Regs::set_flag(u8 i, bool val){
+    set(FLAG, (get(FLAG) & ~(1 << i)) | (val << i));
+}
+
+template <GTE::LimiterType T, u8 V>
+u64 GTE::apply_lim(u64 x){
+    s64 l=0, r;
+    if constexpr (T == AS){
+        l = -(1<<15);
+        r = (1<<15) - 1;
+    } else if constexpr (T == AU){
+        r = (1<<15) - 1;
+    } else if constexpr (T == B){
+        r = (1<<8) - 1;
+    } else if constexpr (T == C){
+        r = (1<<16) - 1;
+    } else if constexpr (T == D){
+        l = -(1 << 10);
+        r = (1 << 10) - 1; 
+    } else if constexpr (T == E){
+        r = (1 << 12) - 1;
+    } else {
+        assert(false);
+    }
+
+    s64 x_s = x;
+    s64 val = std::clamp(x_s, l, r);
+
+    bool fail = false;
+    if (val != x_s){
+        fail = true;
+    }
+
+    if (!fail){
+        return val;
+    }
+
+    if constexpr (T == AS || T == AU){
+        static_assert(1 <= V && V <= 3);
+        m_regs.set_flag(25 - V, true);
+    } else if constexpr (T == B){
+        static_assert(1 <= V && V <= 3);
+        m_regs.set_flag(22 - V, true);
+    } else if constexpr (T == C){
+        static_assert(V == 1);
+        m_regs.set_flag(18, true);
+    } else if constexpr (T == D){
+        static_assert(1 <= V && V <= 2);
+        m_regs.set_flag(15 - V, true);
+    } else if constexpr (T == E){
+        static_assert(V == 1);
+        m_regs.set_flag(12, true);
+    }
+
+    return val;
+}
+
+// begin opcode implementation
+
+#define 
 
 
 
--- src/GTE.hpp
+++ src/GTE.hpp
@@ -7,6 +7,7 @@
 
 namespace pse {
 
+// the GTE design doc is very necessary for understanding naming, etc here
 class GTE {
 public:
 
@@ -21,117 +22,134 @@ private:
             bf32<16, 31> hi;
         };
 
-        static constexpr u32 REGNAME_PACK16 = 1 << 9;
-        static constexpr u32 REGNAME_PACK16_IS_HI = 1 << 10;
-        static constexpr u32 REGNAME_PACK16_HI = REGNAME_PACK16_IS_HI | REGNAME_PACK16;
-        static constexpr u32 REGNAME_PACK16_LO = REGNAME_PACK16;
+
+        static constexpr u32 REGNAME_IND_MASK = 0xFF;
+        static constexpr u32 REGNAME_16 = 1 << 9;
+        static constexpr u32 REGNAME_16_IS_HI = 1 << 10;
+        static constexpr u32 REGNAME_16HI = REGNAME_16_IS_HI | REGNAME_16;
+        static constexpr u32 REGNAME_16LO = REGNAME_16;
+        // there are more signed regs than unsigned
+        // Since we do math in u64, these determine when to sign extend
+        // Math is done in u64 because some results will go over 2^32
+        // this way, can can have absolutely no casting in opcode implementations
+        // which should be nicer since there are a ton of things to implement
+        // casting would be yet another source of bugs
+        static constexpr u32 REGNAME_U = 1 << 11; 
+        static constexpr u32 REGNAME_RONLY = 1 << 12;
+        static constexpr u32 REGNAME_WONLY = 1 << 13;
+
 
         enum RegName : u32 {
             VXY0 = 0,
-            VX0 = 0 | REGNAME_PACK16_LO, VY0 = 0 | REGNAME_PACK16_HI,
-            VZ0 = 1,
-            VX1 = 2 | REGNAME_PACK16_LO, VY1 = 2 | REGNAME_PACK16_HI,
-            VZ1 = 3,
-            VX2 = 4 | REGNAME_PACK16_LO, VY2 = 4 | REGNAME_PACK16_HI,
-            VZ2 = 5,
-            RGBC = 6,
-            OTZ = 7,
-            IR0 = 8,
-            IR1 = 9,
-            IR2 = 10,
-            IR3 = 11,
+            VX0 = 0 | REGNAME_16LO, VY0 = 0 | REGNAME_16HI,
+            VZ0 = 1 | REGNAME_16LO,
+            VXY1 = 2,
+            VX1 = 2 | REGNAME_16LO, VY1 = 2 | REGNAME_16HI,
+            VZ1 = 3 | REGNAME_16LO,
+            VXY2 = 4,
+            VX2 = 4 | REGNAME_16LO, VY2 = 4 | REGNAME_16HI,
+            VZ2 = 5 | REGNAME_16LO,
+            RGBC = 6 | REGNAME_U,
+            OTZ = 7 | REGNAME_16LO | REGNAME_U | REGNAME_RONLY,
+            IR0 = 8 | REGNAME_16LO,
+            IR1 = 9 | REGNAME_16LO,
+            IR2 = 10 | REGNAME_16LO,
+            IR3 = 11 | REGNAME_16LO,
             SXY0 = 12,
-            SX0 = 12 | REGNAME_PACK16_LO, SY0 = 12 | REGNAME_PACK16_HI,
+            SX0 = 12 | REGNAME_16LO, SY0 = 12 | REGNAME_16HI,
             SXY1 = 13,
-            SX1 = 13 | REGNAME_PACK16_LO, SY1 = 13 | REGNAME_PACK16_HI,
+            SX1 = 13 | REGNAME_16LO, SY1 = 13 | REGNAME_16HI,
             SXY2 = 14,
-            SX2 = 14 | REGNAME_PACK16_LO, SY2 = 14 | REGNAME_PACK16_HI,
-            SXYP = 15,
-            SXP = 15 | REGNAME_PACK16_LO, SYP = 15 | REGNAME_PACK16_HI,
-            SZ0 = 16,
-            SZ1 = 17,
-            SZ2 = 18,
-            SZ3 = 19,
-            RGB0 = 20,
-            RGB1 = 21,
-            RGB2 = 22,
+            SX2 = 14 | REGNAME_16LO, SY2 = 14 | REGNAME_16HI,
+            SXYP = 15 | REGNAME_WONLY,
+            SXP = 15 | REGNAME_16LO | REGNAME_WONLY, SYP = 15 | REGNAME_16HI | REGNAME_WONLY,
+            SZ0 = 16 | REGNAME_16LO | REGNAME_U,
+            SZ1 = 17 | REGNAME_16LO | REGNAME_U,
+            SZ2 = 18 | REGNAME_16LO | REGNAME_U,
+            SZ3 = 19 | REGNAME_16LO | REGNAME_U,
+            RGB0 = 20 | REGNAME_U,
+            RGB1 = 21 | REGNAME_U,
+            RGB2 = 22 | REGNAME_U,
             // 23 is unused
-            MAC0 = 24,
+            MAC0 = 24 | REGNAME_RONLY,
             MAC1 = 25,
             MAC2 = 26,
             MAC3 = 27,
-            IRGB = 28,
-            ORGB = 29,
-            LZCS = 30,
-            LZCR = 31,
+            IRGB = 28 | REGNAME_16LO | REGNAME_U,
+            ORGB = 29 | REGNAME_16LO | REGNAME_U,
+            LZCS = 30 | REGNAME_WONLY,
+            LZCR = 31 | REGNAME_RONLY,
             // begin control regs
-            RT11 = 32 | REGNAME_PACK16_LO, RT12 = 32 | REGNAME_PACK16_HI,
-            RT13 = 33 | REGNAME_PACK16_LO, RT21 = 33 | REGNAME_PACK16_HI,
-            RT22 = 34 | REGNAME_PACK16_LO, RT23 = 34 | REGNAME_PACK16_HI,
-            RT31 = 35 | REGNAME_PACK16_LO, RT32 = 35 | REGNAME_PACK16_HI,
-            RT33 = 36,
+            RT11 = 32 | REGNAME_16LO, RT12 = 32 | REGNAME_16HI,
+            RT13 = 33 | REGNAME_16LO, RT21 = 33 | REGNAME_16HI,
+            RT22 = 34 | REGNAME_16LO, RT23 = 34 | REGNAME_16HI,
+            RT31 = 35 | REGNAME_16LO, RT32 = 35 | REGNAME_16HI,
+            RT33 = 36 | REGNAME_16LO,
             TRX = 37,
             TRY = 38,
             TRZ = 39,
-            L11 = 40 | REGNAME_PACK16_LO, L12 = 40 | REGNAME_PACK16_HI,
-            L13 = 41 | REGNAME_PACK16_LO, L21 = 41 | REGNAME_PACK16_HI,
-            L22 = 42 | REGNAME_PACK16_LO, L23 = 42 | REGNAME_PACK16_HI,
-            L31 = 43 | REGNAME_PACK16_LO, L32 = 43 | REGNAME_PACK16_HI,
-            L33 = 44,
+            L11 = 40 | REGNAME_16LO, L12 = 40 | REGNAME_16HI,
+            L13 = 41 | REGNAME_16LO, L21 = 41 | REGNAME_16HI,
+            L22 = 42 | REGNAME_16LO, L23 = 42 | REGNAME_16HI,
+            L31 = 43 | REGNAME_16LO, L32 = 43 | REGNAME_16HI,
+            L33 = 44 | REGNAME_16LO,
             RBK = 45,
             GBK = 46,
             BBK = 47,
-            LR1 = 48 | REGNAME_PACK16_LO, LR2 = 48 | REGNAME_PACK16_HI,
-            LR3 = 49 | REGNAME_PACK16_LO, LG1 = 49 | REGNAME_PACK16_HI,
-            LG2 = 50 | REGNAME_PACK16_LO, LG3 = 50 | REGNAME_PACK16_HI,
-            LB1 = 51 | REGNAME_PACK16_LO, LB2 = 51 | REGNAME_PACK16_HI,
-            LB3 = 52,
+            LR1 = 48 | REGNAME_16LO, LR2 = 48 | REGNAME_16HI,
+            LR3 = 49 | REGNAME_16LO, LG1 = 49 | REGNAME_16HI,
+            LG2 = 50 | REGNAME_16LO, LG3 = 50 | REGNAME_16HI,
+            LB1 = 51 | REGNAME_16LO, LB2 = 51 | REGNAME_16HI,
+            LB3 = 52 | REGNAME_16LO,
             RFC = 53,
             GFC = 54,
             BFC = 55,
             OFX = 56,
             OFY = 57,
-            H = 58,
-            DQA = 59,
+            H = 58 | REGNAME_16LO | REGNAME_U,
+            DQA = 59 | REGNAME_16LO,
             DQB = 60,
-            ZSF3 = 61,
-            ZSF4 = 62,
-            FLAG = 63
+            ZSF3 = 61 | REGNAME_16LO,
+            ZSF4 = 62 | REGNAME_16LO,
+            FLAG = 63 | REGNAME_U
         };
-        
-        union Flag {
-            u32 val;
+        constexpr u32 regname_ind(RegName r);
+        constexpr bool regname_is_16(RegName r);
+        constexpr bool regname_is_16lo(RegName r);
+        constexpr bool regname_is_16hi(RegName r);
+        constexpr bool regname_is_u(RegName r);
+        constexpr bool regname_eq(RegName a, RegName b);
 
-            // naming comes from the psy q doc!!
-            b32<12> ir0_sat;
-            b32<13> sy2_sat;
-            b32<14> sx2_sat;
-            b32<15> mac0_neg;
-            b32<16> mac0_pos;
-            b32<17> div_ovf;
-            b32<18> sz3_otz_sat;
-            b32<19> r_sat;
-            b32<20> g_sat;
-            b32<21> b_sat;
-            b32<22> ir3_sat;
-            b32<23> ir2_sat;
-            b32<24> ir1_sat;
-            b32<25> mac3_neg;
-            b32<26> mac2_neg;
-            b32<27> mac1_neg;
-            b32<28> mac3_pos;
-            b32<29> mac2_pos;
-            b32<30> mac1_pos;
-            b32<31> err;
-        };
+        // direct interface for raw array
+        // NO side effects, literally just raw[i & 0xFF] (because we use upper
+        // bits for attributes)
+        constexpr void set(RegName r, u32 val);
+        constexpr u32 get(RegName r);
 
-        constexpr void shift_SXY();
-        constexpr void shift_SZ();
+        constexpr bool get_flag(u8 i);
+        constexpr void set_flag(u8 i, bool val);
 
+        // TODO perm/noperm variants?
+        // reads and writes may have side effects
         constexpr u32 read(RegName r);
-        // does have special push queue sideeff for SX/YP, hence write
+        // Always use read64 in opcode implementation, for math
+        // This way, no manual casting logic is required
+        constexpr u64 read64(RegName r);
         constexpr void write(RegName r, u32 val);
+
+        // rw sideffs
+        constexpr void shift_SXYP();
+        constexpr void calc_IRGB();
+        constexpr void calc_ORGB();
+        constexpr void calc_LZCS();
+
+        // these are  NOT a builtin sideff, must call
+        constexpr void shift_SXY2();
+        constexpr void shift_SZ3();
+        constexpr void shift_RGBC();
     };
+
+    Regs m_regs;
 
     struct Instr {
         u32 val;
@@ -143,6 +161,18 @@ private:
         bf32<17, 18> mult_mat;
         bf32<19, 19> sf;
     };
+
+    enum LimiterType {
+        AS,
+        AU,
+        B,
+        C,
+        D,
+        E
+    };
+
+    template <LimiterType L, u8 V>
+    u64 apply_lim(u64 x);
 
     using OpHandlerPtr = void (GTE::*)(Instr);
     static const std::array<OpHandlerPtr, 32> m_op_table;
