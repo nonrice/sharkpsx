76907c0e8b31ded5f774c0ed7b3d3bc52bd6b738
Eric Xue <widefin@gmail.com>
2026-06-21 22:50:09 -0400

    back to normal

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,20 +2,14 @@
 #include "App.hpp"
 
 int main(int argc, char** argv){
-    pse::App app{};
-    app.init();
-    app.run();
+    pse::System sys{};
+    sys.set_tty(&std::cout);
 
-    return 0;
+    pse::Debugger app(sys);
 
-//     pse::System sys{};
-//     sys.set_tty(&std::cout);
-
-//     pse::Debugger app(sys);
-
-//     if (argc == 1){
-//         app.run();
-//     } else {
-//         app.run_file(std::string(argv[1]));
-//     }
+    if (argc == 1){
+        app.run();
+    } else {
+        app.run_file(std::string(argv[1]));
+    }
 }
